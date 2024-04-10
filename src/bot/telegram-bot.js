9c805b0562ba48ba11aa20cb71a65d736d9b0b54
kevinderitis <kevin.deritis@hotmail.com>
2024-04-10 00:44:01 -0300

    New implementation

--- src/bot/telegram-bot.js
+++ src/bot/telegram-bot.js
@@ -9,79 +9,45 @@ const bot = new TelegramBot(token, { polling: true });
 const prepareCards = texto => {
     let resultado = [];
 
-    const regexCarta1 = /\*Carta1: ([^\*]+)/;
-    const regexCarta2 = /\*Carta2: ([^\*]+)/;
-    const regexCarta3 = /\*Carta3: ([^\*]+)/;
-    const regexDesc1 = /\*Descripcion1: ([^\*]+)/;
-    const regexDesc2 = /\*Descripcion2: ([^\*]+)/;
-    const regexDesc3 = /\*Descripcion3: ([^\*]+)/;
-
-    const matchCarta1 = texto.match(regexCarta1);
-    const matchCarta2 = texto.match(regexCarta2);
-    const matchCarta3 = texto.match(regexCarta3);
-    const matchDesc1 = texto.match(regexDesc1);
-    const matchDesc2 = texto.match(regexDesc2);
-    const matchDesc3 = texto.match(regexDesc3);
-
-    if (matchCarta1 || matchCarta2 || matchCarta3 || matchDesc1 || matchDesc2 || matchDesc3) {
-
-        const regexCarta1 = /\*Carta1: ([^\*]+)/;
-        const regexCarta2 = /\*Carta2: ([^\*]+)/;
-        const regexCarta3 = /\*Carta3: ([^\*]+)/;
-        const regexDesc1 = /\*Descripcion1: ([^\*]+)/;
-        const regexDesc2 = /\*Descripcion2: ([^\*]+)/;
-        const regexDesc3 = /\*Descripcion3: ([^\*]+)/;
-
-        const matchCarta1 = texto.match(regexCarta1);
-        const matchCarta2 = texto.match(regexCarta2);
-        const matchCarta3 = texto.match(regexCarta3);
-        const matchDesc1 = texto.match(regexDesc1);
-        const matchDesc2 = texto.match(regexDesc2);
-        const matchDesc3 = texto.match(regexDesc3);
-
-        const indiceCarta1 = texto.indexOf('*Carta1');
-        const indiceCarta2 = texto.indexOf('*Carta2');
-        const indiceCarta3 = texto.indexOf('*Carta3');
-        const indiceTexto2 = texto.length;
-
-        const texto1Prov = texto.substring(0, indiceCarta1);
-        const texto2Prov = texto.substring(0, indiceCarta2);
-        const texto3Prov = texto.substring(0, indiceCarta3);
-        let texto2 = '';
-
-        if (matchCarta1) {
-            resultado.push({
-                texto: texto1Prov.trim()
-            });
-
-            resultado.push({
-                carta: matchCarta1[1].trim(),
-                texto: matchDesc1 ? matchDesc1[1].trim() : ''
-            });
-        };
-
-        if (matchCarta2) {
-            resultado.push({
-                texto: texto2Prov.trim()
-            });
-            resultado.push({
-                carta: matchCarta2[1].trim(),
-                texto: matchDesc2 ? matchDesc2[1].trim() : ''
-            });
-
-        };
-
-        if (matchCarta3) {
-            resultado.push({
-                texto: texto3Prov.trim()
-            });
-            resultado.push({
-                carta: matchCarta3[1].trim(),
-                texto: matchDesc3 ? matchDesc3[1].trim() : ''
-            });
-        };
-
+    let indiceCarta1 = texto.indexOf('Carta1:');
+    if (indiceCarta1 === -1) {
+        let indiceCarta2 = texto.indexOf('Carta2:');
+        if (indiceCarta2 === -1) {
+            let indiceCarta3 = texto.indexOf('Carta3:');
+            if (indiceCarta3 === -1) {
+                resultado.push({ texto });
+                return resultado;
+            } else {
+                indiceCarta1 = indiceCarta3; 
+            }
+        } else {
+            indiceCarta1 = indiceCarta2; 
+        }
     }
+
+    let textoAntesCartas = texto.substring(0, (indiceCarta1 - 4)).trim();
+    resultado.push({ texto: textoAntesCartas });
+
+    for (let i = 1; i <= 3; i++) {
+        let cartaKey = `Carta${i}:`;
+        let descKey = `Descripcion${i}:`;
+
+        let indiceCarta = texto.indexOf(cartaKey, indiceCarta1);
+        let indiceDesc = texto.indexOf(descKey, indiceCarta);
+
+        if(indiceDesc === -1){
+            let descKey = `Descripción${i}:`;
+            indiceDesc = texto.indexOf(descKey, indiceCarta);
+        };
+
+        if (indiceCarta !== -1 && indiceDesc !== -1) {
+            let carta = texto.substring(indiceCarta + cartaKey.length, (indiceDesc - 2)).trim();
+            let desc = texto.substring(indiceDesc + descKey.length).trim();
+
+            resultado.push({ carta, texto: desc });
+        }
+    }
+
     return resultado;
 };
 
@@ -91,6 +57,7 @@ const sendMultipleMessages = (chatId, bot, mensajes, tiempoDeEspera) => {
             let msg = mensaje.carta ? `La carta es: ${mensaje.carta} ${mensaje.texto}` : mensaje.texto;
             bot.sendMessage(chatId, msg);
         }, tiempoDeEspera * index);
+        bot.sendChatAction(chatId, 'typing');
     });
 };
 
@@ -108,13 +75,16 @@ export const initBot = () => {
             console.log(error)
         }
 
+        bot.sendChatAction(chatId, 'typing');
         let cards = prepareCards(response);
-        let delay = 10000;
+        let delay = 20000;
 
-        if (cards.length > 0) {
+        if (cards.length > 1) {
             sendMultipleMessages(chatId, bot, cards, delay)
         } else {
-            bot.sendMessage(chatId, response);
+            setTimeout(() => {
+                bot.sendMessage(chatId, response);
+              }, 8000);
         }
 
 
@@ -138,3 +108,4 @@ export const initBot = () => {
     console.log('Bot listo para recibir mensajes...');
 
 };
+
